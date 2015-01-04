d31a37e2ec996902462f6ae3a3fba1bddd3e4292
JP Smith <jp.smith@ilstu.edu>
2015-01-03 21:20:00 -0600

    cleaned up leftovers

--- sigmoid_a.m
+++ sigmoid_a.m
@@ -1,6 +1,4 @@
 function g = sigmoid_a(z)
-%SIGMOID Compute sigmoid function using atan
-%   J = SIGMOID(z) computes the sigmoid of z.
 
 g = (1 / pi) * atan (z) + .5;
 
--- sigmoid_l.m
+++ sigmoid_l.m
@@ -1,6 +1,4 @@
 function g = sigmoid_l(z)
-%SIGMOID Compute sigmoid function using logit
-%   J = SIGMOID(z) computes the sigmoid of z.
 
 g = 1 ./ (1 + exp(-z));
 
